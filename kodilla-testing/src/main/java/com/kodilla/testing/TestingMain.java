6ae9f7147ec98eecf2930d6a7396fccd203d1b3d
stereochild <kamil.drozdz@gmail.com>
2017-08-02 18:46:09 +0200

    Third Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,43 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+     //example test
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println(("test OK"));
+        } else {
+            System.out.println(("Error!"));
+        }
+
+
+    // calculator test
+
+        Calculator simpleCalculator = new Calculator();
+
+        int add = simpleCalculator.addAToB(10, 10);
+        int predictedSum = 20;
+
+        if (add == predictedSum) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("ERROR!");
+        }
+
+        int susbtract = simpleCalculator.substractBFromA(10, 10);
+        int predictedDifference = 0;
+
+        if (susbtract == predictedDifference) {
+            System.out.println("test OK");
+        } else {
+            System.out.println(("ERROR!"));
+        }
     }
 }
