8e04fceb960275117048a91e70d12c429f6e6775
stereochild <kamil.drozdz@gmail.com>
2017-08-09 23:06:07 +0200

    7.1 fixing lambdas

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemMain.java
@@ -11,8 +11,8 @@ public class PoemMain {
         AnotherPoemBeautifier anotherPoemBeutifier = new AnotherPoemBeautifier();
         anotherPoemBeutifier.beautifyAgain("There's a lady who's sure all that glitters is gold", poem -> poem + "...");
         anotherPoemBeutifier.beautifyAgain("And she's buying a stairway to heaven", poem -> "..." + poem + "...");
-        anotherPoemBeutifier.beautifyAgain("WHEN SHE GETS THERE SHE KNOWS, IF THE STORES ARE ALL CLOSED", poem -> "..." + toLowerCase(poem) + "...");
+        anotherPoemBeutifier.beautifyAgain("WHEN SHE GETS THERE SHE KNOWS, IF THE STORES ARE ALL CLOSED", poem -> "..." + poem.toLowerCase() + "...");
         anotherPoemBeutifier.beautifyAgain("a word she can get what she came for.", poem -> "With " + poem);
-        anotherPoemBeutifier.beautifyAgain("and she's buying a stairway to heaven", poem -> "OHH, OHH " + toUpperCase(poem));
+        anotherPoemBeutifier.beautifyAgain("and she's buying a stairway to heaven", poem -> "OHH, OHH " + poem.toUpperCase());
     }
 }
