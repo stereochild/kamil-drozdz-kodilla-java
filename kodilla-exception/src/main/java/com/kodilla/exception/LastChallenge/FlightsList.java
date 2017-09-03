ca54466e6b31b03201625eb383b5ff42c3487426
stereochild <kamil.drozdz@gmail.com>
2017-09-03 17:48:10 +0200

    8.4 removing map to constructor

--- kodilla-exception/src/main/java/com/kodilla/exception/LastChallenge/FlightsList.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/LastChallenge/FlightsList.java
@@ -5,8 +5,9 @@ import java.util.Map;
 
 public class FlightsList{
 
-    public boolean findFlight(Flight flight) throws RouteNotFoundException  {
-        Map<String, Boolean> flights = new HashMap<>();
+    public static final Map<String, Boolean> flights = new HashMap<>();
+
+    static {
         flights.put("Warsaw", true);
         flights.put("Radom", true);
         flights.put("Modlin", true);
@@ -15,10 +16,14 @@ public class FlightsList{
         flights.put("Gdansk", true);
         flights.put("Wroclaw", true);
         flights.put("Ciechocinek", false);
+    }
+
+    public boolean findFlight(Flight flight) throws RouteNotFoundException  {
 
         if(!flights.containsKey(flight.getDepartureAirport()) || !flights.containsKey(flight.getArrivalAirport())){
         throw new RouteNotFoundException("Flight not found.");
         }
+
         return flights.get(flight.getDepartureAirport()) & flights.get(flight.getArrivalAirport());
     }
 }
