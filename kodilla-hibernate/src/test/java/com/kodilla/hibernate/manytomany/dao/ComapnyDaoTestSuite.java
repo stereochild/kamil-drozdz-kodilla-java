86e8737e08e8f4b9080a7c3e3735f9230761e2e2
stereochild <kamil.drozdz@gmail.com>
2017-10-03 00:40:01 +0200

    Playing with tests

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/ComapnyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/ComapnyDaoTestSuite.java
@@ -75,23 +75,28 @@ public class ComapnyDaoTestSuite {
         Company dataMaesters = new Company("Data Maesters");
 
         johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(dataMaesters);
         stephanieClarkson.getCompanies().add(softwareMachine);
         softwareMachine.getEmployees().add(johnSmith);
         softwareMachine.getEmployees().add(stephanieClarkson);
+        dataMaesters.getEmployees().add(johnSmith);
 
         employeeDao.save(johnSmith);
-        int johnId = johnSmith.getId();
+        int smithId = johnSmith.getId();
         employeeDao.save(stephanieClarkson);
-        int stephanieId = stephanieClarkson.getId();
+        int clarksonId = stephanieClarkson.getId();
 
         //When
         List<Employee> employeeLastname = employeeDao.retrieveEmployeeByLastname("Smith");
         //Then
         Assert.assertEquals(1, employeeLastname.size());
         //CleanUp
-        employeeDao.delete(johnId);
-        employeeDao.delete(stephanieId);
-
+        try {
+            employeeDao.delete(smithId);
+            employeeDao.delete(clarksonId);
+        } catch (Exception e) {
+            //do nothing
+        }
     }
 
     @Test
@@ -104,8 +109,10 @@ public class ComapnyDaoTestSuite {
         Company dataMaesters = new Company("Data Maesters");
 
         johnSmith.getCompanies().add(softwareMachine);
-        softwareMachine.getEmployees().add(johnSmith);
         johnSmith.getCompanies().add(dataMaesters);
+        stephanieClarkson.getCompanies().add(softwareMachine);
+        softwareMachine.getEmployees().add(johnSmith);
+        softwareMachine.getEmployees().add(stephanieClarkson);
         dataMaesters.getEmployees().add(johnSmith);
 
         companyDao.save(softwareMachine);
@@ -114,11 +121,16 @@ public class ComapnyDaoTestSuite {
         int dataId = dataMaesters.getId();
 
         //When
-        List<Company> companyName = companyDao.retrieveCompanyByName("Sof");
+        List<Company> companyName = companyDao.retrieveCompanyByName("Dat");
+
         //Then
         Assert.assertEquals(1, companyName.size());
         //CleanUp
-        companyDao.delete(softwareId);
-        companyDao.delete(dataId);
+        try {
+            companyDao.delete(softwareId);
+            companyDao.delete(dataId);
+        } catch (Exception e) {
+            //do nothing
+        }
     }
 }
