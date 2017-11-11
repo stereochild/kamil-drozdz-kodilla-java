3b8134d1c30aaffd55d6c876ed49f4b6dd5bb124
stereochild <kamil.drozdz@gmail.com>
2017-11-11 12:46:18 +0100

    20.2 first commit

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/decorator/pizza/Meat.java
@@ -0,0 +1,19 @@
+package com.kodilla.patterns2.decorator.pizza;
+
+import java.math.BigDecimal;
+
+public class Meat extends AbstractPizzaOrderDecorator {
+    public Meat(PizzaOrder pizzaOrder) {
+        super(pizzaOrder);
+    }
+
+    @Override
+    public BigDecimal getCost() {
+        return super.getCost().add(new BigDecimal(6));
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + " + meat";
+    }
+}
---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/fascade/decorator/pizza/PizzaOrderTestSuite.java
@@ -0,0 +1,109 @@
+package com.kodilla.patterns2.fascade.decorator.pizza;
+
+import com.kodilla.patterns2.decorator.pizza.*;
+import org.junit.Test;
+import java.math.BigDecimal;
+import static org.junit.Assert.assertEquals;
+
+public class PizzaOrderTestSuite {
+
+    @Test
+    public void testBasicPizzaOrderGetCost() {
+        //Given
+        PizzaOrder theOrder = new BasicPizzaOrder();
+        System.out.println(theOrder.getCost());
+        //When
+        BigDecimal theCost = theOrder.getCost();
+        //Then
+        assertEquals(new BigDecimal(15), theCost);
+    }
+
+    @Test
+    public void testBasicPizzaOrderGetDescription() {
+        //Given
+        PizzaOrder theOrder = new BasicPizzaOrder();
+        System.out.println(theOrder.getDescription());
+        //When
+        String description = theOrder.getDescription();
+        //Then
+        assertEquals("Pizza with tomato sauce and cheese", description);
+    }
+
+    @Test
+    public void testExpressDeliveryGetCost() {
+        //Given
+        PizzaOrder theOrder = new BasicPizzaOrder();
+        theOrder = new ExpressDelivery(theOrder);
+        System.out.println(theOrder.getCost());
+        //When
+        BigDecimal theCost = theOrder.getCost();
+        //Then
+        assertEquals(new BigDecimal(25), theCost);
+    }
+
+    @Test
+    public void testExpressDeliveryGetDescription() {
+        //Given
+        PizzaOrder theOrder = new BasicPizzaOrder();
+        theOrder = new ExpressDelivery(theOrder);
+        System.out.println(theOrder.getDescription());
+        //When
+        String description = theOrder.getDescription();
+        //Then
+        assertEquals("Pizza with tomato sauce and cheese with express delivery", description);
+    }
+
+    @Test
+    public void testThickCrustAndExtraSauceGetCost() {
+        //Given
+        PizzaOrder theOrder = new BasicPizzaOrder();
+        theOrder = new ThickCrust(theOrder);
+        theOrder = new ExtraSauce(theOrder);
+        System.out.println(theOrder.getCost());
+        //When
+        BigDecimal theCost = theOrder.getCost();
+        //Then
+        assertEquals(new BigDecimal(20), theCost);
+    }
+
+    @Test
+    public void testThickCrustAndExtraSauceGetDescription() {
+        //Given
+        PizzaOrder theOrder = new BasicPizzaOrder();
+        theOrder = new ThickCrust(theOrder);
+        theOrder = new ExtraSauce(theOrder);
+        System.out.println(theOrder.getDescription());
+        //When
+        String description = theOrder.getDescription();
+        //Then
+        assertEquals("Pizza with tomato sauce and cheese on thick crust + extra sauce", description);
+    }
+
+    @Test
+    public void testAllToppingsGetCost() {
+        //Given
+        PizzaOrder theOrder = new BasicPizzaOrder();
+        theOrder = new ExtraCheese(theOrder);
+        theOrder = new Vegatables(theOrder);
+        theOrder = new Meat(theOrder);
+        System.out.println(theOrder.getCost());
+        //When
+        BigDecimal theCost = theOrder.getCost();
+        //Then
+        assertEquals(new BigDecimal(30), theCost);
+    }
+
+    @Test
+    public void testAllToppingsGetDescription() {
+        //Given
+        PizzaOrder theOrder = new BasicPizzaOrder();
+        theOrder = new ExtraCheese(theOrder);
+        theOrder = new Vegatables(theOrder);
+        theOrder = new Meat(theOrder);
+        System.out.println(theOrder.getDescription());
+        //When
+        String description = theOrder.getDescription();
+        //Then
+        assertEquals("Pizza with tomato sauce and cheese + extra cheese + vegatables + meat", description);
+    }
+}
